2855c42d5dc6970bbbeaba6b71eb49bb0f825710
Jacob Kjærager <jacob.7120@gmail.com>
2021-12-06 16:42:05 +0100

    Updated more configuration to hyperopt_models

--- hyperopt_models.py
+++ hyperopt_models.py
@@ -4,6 +4,333 @@ import numpy as np
 
 def get_hyper_opt_conf() -> list:
 	return [
+    {
+		"enabled_for_run": True,
+		"EPOCHS": 1,
+		"input_shape_sample": 100,
+		"input_shape_features": 40,
+		"data_dimensions": 3,
+		"model": "sequential",
+		"optimizer": "adam",
+		"loss_function": "categorical_crossentropy",
+		"layers": [
+			{
+				"layer_type": "lstm",
+				"layer_arguments": {
+					"units": 50,
+					"activation": "tanh",
+					"return_sequences": True,
+					"input_shape": (100, 40)
+				}
+			},
+			{
+				"layer_type": "lstm",
+				"layer_arguments": {
+					"units": 50,
+					"activation": "tanh"
+				},
+			},
+			{
+				"layer_type": "dense",
+				"layer_arguments": {
+						"units": 3,
+						"activation": "softmax"
+				}
+			}
+		]
+	},
+        {
+		"enabled_for_run": True,
+		"EPOCHS": 1,
+		"input_shape_sample": 100,
+		"input_shape_features": 40,
+		"data_dimensions": 3,
+		"model": "sequential",
+		"optimizer": "adam",
+		"loss_function": "categorical_crossentropy",
+		"layers": [
+			{
+				"layer_type": "lstm",
+				"layer_arguments": {
+					"units": 80,
+					"activation": "tanh",
+					"return_sequences": True,
+					"input_shape": (100, 40)
+				}
+			},
+			{
+				"layer_type": "lstm",
+				"layer_arguments": {
+					"units": 80,
+					"activation": "tanh"
+				},
+			},
+			{
+				"layer_type": "dense",
+				"layer_arguments": {
+						"units": 3,
+						"activation": "softmax"
+				}
+			}
+		]
+	},
+    {
+		"enabled_for_run": True,
+		"EPOCHS": 1,
+		"input_shape_sample": 100,
+		"input_shape_features": 40,
+		"data_dimensions": 3,
+		"model": "sequential",
+		"optimizer": "adam",
+		"loss_function": "categorical_crossentropy",
+		"layers": [
+			{
+				"layer_type": "lstm",
+				"layer_arguments": {
+					"units": 120,
+					"activation": "tanh",
+					"return_sequences": True,
+					"input_shape": (100, 40)
+				}
+			},
+			{
+				"layer_type": "lstm",
+				"layer_arguments": {
+					"units": 120,
+					"activation": "tanh"
+				},
+			},
+			{
+				"layer_type": "dense",
+				"layer_arguments": {
+						"units": 3,
+						"activation": "softmax"
+				}
+			}
+		]
+	},    
+    {
+		"enabled_for_run": True,
+		"EPOCHS": 1,
+		"input_shape_sample": 100,
+		"input_shape_features": 40,
+		"data_dimensions": 3,
+		"model": "sequential",
+		"optimizer": "adam",
+		"loss_function": "categorical_crossentropy",
+		"layers": [
+			{
+				"layer_type": "lstm",
+				"layer_arguments": {
+					"units": 50,
+					"activation": "tanh",
+                    "recurrent_activation":"sigmoid",
+					"return_sequences": True,
+					"input_shape": (100, 40)
+				}
+			},
+			{
+				"layer_type": "lstm",
+				"layer_arguments": {
+					"units": 50,
+					"activation": "tanh"
+				},
+			},
+			{
+				"layer_type": "dense",
+				"layer_arguments": {
+						"units": 3,
+						"activation": "softmax"
+				}
+			}
+		]
+	},
+    {
+		"enabled_for_run": True,
+		"EPOCHS": 1,
+		"input_shape_sample": 100,
+		"input_shape_features": 40,
+		"data_dimensions": 3,
+		"model": "sequential",
+		"optimizer": "adam",
+		"loss_function": "categorical_crossentropy",
+		"layers": [
+			{
+				"layer_type": "lstm",
+				"layer_arguments": {
+					"units": 150,
+					"activation": "tanh",
+                    "recurrent_activation":"sigmoid",
+					"return_sequences": True,
+					"input_shape": (100, 40)
+				}
+			},
+			{
+				"layer_type": "lstm",
+				"layer_arguments": {
+					"units": 150,
+					"activation": "tanh"
+				},
+			},
+			{
+				"layer_type": "dense",
+				"layer_arguments": {
+						"units": 3,
+						"activation": "softmax"
+				}
+			}
+		]
+	},
+	{
+		"enabled_for_run": True,
+		"EPOCHS": 1,
+		"input_shape_sample": 100,
+		"input_shape_features": 40,
+		"data_dimensions": 3,
+		"model": "sequential",
+		"optimizer": "adam",
+		"loss_function": "categorical_crossentropy",
+		"layers": [
+			{
+				"layer_type": "lstm",
+				"layer_arguments": {
+					"units": 50,
+					"activation": "tanh",
+					"input_shape": (100, 40)
+				}
+			},
+			{
+				"layer_type": "dense",
+				"layer_arguments": {
+						"units": 3,
+						"activation": "softmax"
+				}
+			}
+		]
+	},
+	{
+		"enabled_for_run": True,
+		"EPOCHS": 1,
+		"input_shape_sample": 100,
+		"input_shape_features": 40,
+		"data_dimensions": 3,
+		"model": "sequential",
+		"optimizer": "adam",
+		"loss_function": "categorical_crossentropy",
+		"layers": [
+			{
+				"layer_type": "lstm",
+				"layer_arguments": {
+					"units": 80,
+					"activation": "tanh",
+					"input_shape": (100, 40)
+				}
+			},
+			{
+				"layer_type": "dense",
+				"layer_arguments": {
+						"units": 3,
+						"activation": "softmax"
+				}
+			}
+		]
+	},
+	{
+		"enabled_for_run": True,
+		"EPOCHS": 1,
+		"input_shape_sample": 100,
+		"input_shape_features": 40,
+		"data_dimensions": 3,
+		"model": "sequential",
+		"optimizer": "adam",
+		"loss_function": "categorical_crossentropy",
+		"layers": [
+			{
+				"layer_type": "lstm",
+				"layer_arguments": {
+					"units": 120,
+					"activation": "tanh",
+					"input_shape": (100, 40)
+				}
+			},
+			{
+				"layer_type": "dense",
+				"layer_arguments": {
+						"units": 3,
+						"activation": "softmax"
+				}
+			}
+		]
+	},
+	{
+		"enabled_for_run": True,
+		"EPOCHS": 1,
+		"input_shape_sample": 100,
+		"input_shape_features": 40,
+		"data_dimensions": 3,
+		"model": "sequential",
+		"optimizer": "adam",
+		"loss_function": "categorical_crossentropy",
+		"layers": [
+			{
+				"layer_type": "lstm",
+				"layer_arguments": {
+					"units": 150,
+					"activation": "tanh",
+					"input_shape": (100, 40)
+				}
+			},
+			{
+				"layer_type": "dense",
+				"layer_arguments": {
+						"units": 3,
+						"activation": "softmax"
+				}
+			}
+		]
+	},
+	{
+		"enabled_for_run": False,
+		"EPOCHS": 100,
+		"input_shape_sample": 100,
+		"input_shape_features": 40,
+		"data_dimensions": 4,
+		"model": "sequential",
+		"optimizer": "adam",
+		"loss_function": "categorical_crossentropy",
+		"layers": [
+			{
+				"layer_type": "conv2d",
+				"layer_arguments": {
+					"filters": 80,
+					"kernel_size": 3,
+					"activation": "tanh",
+					"input_shape": (100, 40, 1)
+				}
+			},
+			{
+				"layer_type": "conv2d",
+				"layer_arguments": {
+					"filters": 40,
+					"kernel_size": 3,
+					"activation": "tanh"
+				}
+			},
+			{
+				"layer_type": "flatten",
+				"layer_arguments": {
+
+				}
+			},
+			{
+				"layer_type": "dense",
+				"layer_arguments": {
+					"units": 3,
+					"activation": "softmax"
+				}
+			}
+		]
+	}
     	{
 		"enabled_for_run": True,
 		"EPOCHS": 100,
@@ -145,110 +472,6 @@ def get_hyper_opt_conf() -> list:
         "hidden_layer_2_shape_1": 120,
         "hidden_layer_2_shape_2": 5
 	},
-	{
-		"enabled_for_run": False,
-		"EPOCHS": 1,
-		"input_shape_sample": 100,
-		"input_shape_features": 40,
-		"data_dimensions": 3,
-		"model": "sequential",
-		"optimizer": "adam",
-		"loss_function": "categorical_crossentropy",
-		"layers": [
-			{
-				"layer_type": "lstm",
-				"layer_arguments": {
-					"units": 50,
-					"activation": "tanh",
-					"return_sequences": True,
-					"input_shape": (100, 40)
-				}
-			},
-			{
-				"layer_type": "lstm",
-				"layer_arguments": {
-					"units": 50,
-					"activation": "tanh"
-				},
-			},
-			{
-				"layer_type": "dense",
-				"layer_arguments": {
-						"units": 3,
-						"activation": "softmax"
-				}
-			}
-		]
-	},
-	{
-		"enabled_for_run": False,
-		"EPOCHS": 1,
-		"input_shape_sample": 100,
-		"input_shape_features": 40,
-		"data_dimensions": 3,
-		"model": "sequential",
-		"optimizer": "adam",
-		"loss_function": "categorical_crossentropy",
-		"layers": [
-			{
-				"layer_type": "lstm",
-				"layer_arguments": {
-					"units": 50,
-					"activation": "tanh",
-					"input_shape": (100, 40)
-				}
-			},
-			{
-				"layer_type": "dense",
-				"layer_arguments": {
-						"units": 3,
-						"activation": "softmax"
-				}
-			}
-		]
-	},
-	{
-		"enabled_for_run": False,
-		"EPOCHS": 1,
-		"input_shape_sample": 100,
-		"input_shape_features": 40,
-		"data_dimensions": 4,
-		"model": "sequential",
-		"optimizer": "adam",
-		"loss_function": "categorical_crossentropy",
-		"layers": [
-			{
-				"layer_type": "conv2d",
-				"layer_arguments": {
-					"filters": 80,
-					"kernel_size": 3,
-					"activation": "tanh",
-					"input_shape": (100, 40, 1)
-				}
-			},
-			{
-				"layer_type": "conv2d",
-				"layer_arguments": {
-					"filters": 40,
-					"kernel_size": 3,
-					"activation": "tanh"
-				}
-			},
-			{
-				"layer_type": "flatten",
-				"layer_arguments": {
-
-				}
-			},
-			{
-				"layer_type": "dense",
-				"layer_arguments": {
-					"units": 3,
-					"activation": "softmax"
-				}
-			}
-		]
-	}
 ]
 
 
